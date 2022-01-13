206a0b03752b2aef62505b847bac65c3bd78d512
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2022-01-13 14:12:22 +0100

    add some js product handling

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,10 +4,37 @@ const getProducts = () => {
         .catch(error => console.log(error));
 }
 
+const createHtmlElementFromString = (htmlAsString) => {
+    let myElement = document.createElement('div');
+    myElement.innerHTML = htmlAsString.trim();
+
+    return myElement.firstChild;
+}
+
+const createProductComponent = (product) => {
+    const template = `
+        <li class="product">
+            <h3>${product.title}</h3>
+            <div class="product__image">
+                <img src="${product.mediaPath}" />
+            </div>
+            <span class="product__price">${product.price}</span>
+            <button
+                class="product__add-to-basket"
+                data-product-id="${product.id}"
+            >Add to basket</button>
+        </li>
+    `;
+
+    return createHtmlElementFromString(template);
+}
+
 (() => {
     console.log("It works :):)");
 
     getProducts()
-        //.then(products => //create html struct with products)
-        .then(products => console.log(products))
+        .then(productsAsJsonObject => productsAsJsonObject.map(createProductComponent))
+        .then(productsAsHtml => console.log(productsAsHtml))
+        //json -> html
+
 })();
